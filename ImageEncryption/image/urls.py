c39efd6f4ef183f5165ea5fbff13f8d2e94cb15b
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-30 14:28:28 +0530

    upload images done

--- ImageEncryption/image/urls.py
+++ ImageEncryption/image/urls.py
@@ -1,6 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('image', views.image, name='image'),
+    path("admin/", admin.site.urls),
+    path("image", views.upload_image, name="image"),
+    path("images", views.get_images, name="images"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
