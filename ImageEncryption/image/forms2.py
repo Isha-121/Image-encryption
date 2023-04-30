b79ec4f52dff8f1adbc60ea29cea8f65f7f1397e
Isha Shah <ishashahjan@gmail.com>
2023-05-01 02:25:00 +0530

    aes decrypt added

--- ImageEncryption/image/forms2.py
+++ ImageEncryption/image/forms2.py
@@ -9,3 +9,7 @@ class AESImageForm(forms.ModelForm):
     class Meta:
         model = AES_Image
         fields = "__all__"
+        widgets={
+            'key': forms.TextInput(attrs={'class': 'form-control','placeholder':'Enter a secret key of image'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
